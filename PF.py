3abe468997af1ad783ef9ee108171484dc3de539
xyh <3190100997@zju.edu.cn>
2022-12-17 22:09:56 +0100

    PF

--- PF.py
+++ PF.py
@@ -2,7 +2,8 @@ import os
 import cv2
 import numpy as np
 import random
-from utils import plot_one_box, cal_iou, xyxy_to_xywh, xywh_to_xyxy, updata_trace_list, draw_trace
+from utils import plot_one_box, cal_iou, xyxy_to_xywh, xywh_to_xyxy, updata_trace_list, draw_trace, calc_center
+import math
 
 # 单目标跟踪
 # 检测器获得检测框，全程只赋予1个ID，有两个相同的东西进来时，不会丢失唯一跟踪目标
@@ -21,7 +22,7 @@ initial_target_box = [729, 238, 764, 339]  # 目标初始bouding box
 
 initial_box_state = xyxy_to_xywh(initial_target_box)
 initial_state = np.array([[initial_box_state[0], initial_box_state[1], initial_box_state[2], initial_box_state[3],0, 0]]).T  # [中心x,中心y,宽w,高h,dx,dy]
-IOU_Threshold = 0.3 # 匹配时的阈值
+IOU_Threshold = 0.2 # 匹配时的阈值
 
 # 状态转移矩阵，上一时刻的状态转移到当前时刻
 A = np.array([[1, 0, 0, 0, 1, 0],
@@ -49,6 +50,7 @@ B = None
 P = np.eye(6)
 # Create x as a list of particle
 particle_number=800
+spreaddis = 10
 x = [0] * (particle_number*2)
 w = [0] * particle_number
 re_sampling_num = int(particle_number)
@@ -62,7 +64,7 @@ def gauss_likelihood(x, sigma):
 if __name__ == "__main__":
 
     video_path = "./data/testvideo1.mp4"
-    label_path = "./data/labels"
+    label_path = "./data/labels/labels"
     file_name = "testvideo1"
     cap = cv2.VideoCapture(video_path)
     # cv2.namedWindow("track", cv2.WINDOW_NORMAL)
@@ -77,11 +79,14 @@ if __name__ == "__main__":
     P_posterior = np.array(P)
     Z = np.array(initial_state)
     trace_list = []  # 用于保存目标box的轨迹
+    dx_list = []
+    dy_list = []
 
     while (True):
         # Capture frame-by-frame
         ret, frame = cap.read()
         last_box_posterior = xywh_to_xyxy(X_posterior[0:4])
+        last_iou = 0
         plot_one_box(last_box_posterior, frame, color=(255, 255, 255), target=False)
         if not ret:
             break
@@ -89,9 +94,13 @@ if __name__ == "__main__":
         label_file_path = os.path.join(label_path, file_name + "_" + str(frame_counter) + ".txt")
         with open(label_file_path, "r") as f:
             content = f.readlines()
+            # if last_iou>0.8:
+            #     max_iou = 0.8
             max_iou = IOU_Threshold
             max_iou_matched = False
             # ---------使用最大IOU来寻找观测值------------
+            match_list = []
+
             for j, data_ in enumerate(content):
                 data = data_.replace('\n', "").split(" ")
                 xyxy = np.array(data[1:5], dtype="float")
@@ -101,6 +110,12 @@ if __name__ == "__main__":
                     target_box = xyxy
                     max_iou = iou
                     max_iou_matched = True
+                match_list.append(iou[0])
+            print(match_list)
+            # if(len([x  for x in match_list if x>0.4])>1):
+            #     print("wowwwww")
+            #     max_iou_matched = False
+            last_iou = max_iou
             if max_iou_matched == True:
                 # 如果找到了最大IOU BOX,则认为该框为观测值
                 plot_one_box(target_box, frame, target=True)
@@ -111,9 +126,10 @@ if __name__ == "__main__":
                             0.7,
                             (255, 0, 0), 2)
                 # 计算dx,dy
-                print(X_posterior)
                 dx = (xywh[0] - X_posterior[0])[0]
                 dy = (xywh[1] - X_posterior[1])[0]
+                dx_list.append(dx)
+                dy_list.append(dy)
 
 
                 
@@ -130,23 +146,30 @@ if __name__ == "__main__":
             print(dx,dy)
             deltax = dx
             deltay = dy
+
+            if(len(dx_list)>5):
+                deltax = dx + 0.5*(dx_list[-1] - dx_list[-2]) +  0.05*(dx_list[-2] - dx_list[-3])
+                deltay = dy + 0.5*(dy_list[-1] - dy_list[-2]) +  0.05*(dy_list[-2] - dy_list[-3])
+
             for i in range(len(x_bar)//2):
-                if x_bar[i*2]+ deltax <768:
-                    x_bar[i*2]+=deltax
-                if x_bar[i*2+1]+ deltay <576:
-                    x_bar[i*2+1]+= deltay
+                sx = int(random.gauss(0,1)*spreaddis)
+                sy = int(random.gauss(0,1)*spreaddis)
+                if x_bar[i*2]+ deltax+sx <768:
+                    x_bar[i*2]+=deltax+sx
+                if x_bar[i*2+1]+ deltay+sy <576:
+                    x_bar[i*2+1]+= deltay+sy
                   
             #calculate weight
             for i in range(len(x_bar)//2):
                 if target_box[0]<x_bar[i*2]<target_box[2] and target_box[1]<x_bar[i*2+1]<target_box[3]:
                     
-                    w[i]=0.99
+                    w[i]=1
                 else:
-                    w[i]=0.01
+                    w[i]=0.001
                     
             #normalize weight
             sumx = sum(w)
-            w=[x/sumx for x in w]
+            w = [x/sumx for x in w]
             
             #re-sampling
             xr=random.uniform(0, 1)/re_sampling_num
@@ -163,8 +186,10 @@ if __name__ == "__main__":
             
             
             for i in range(re_sampling_num):
-                x.append(x_bar[re_sample_index[i]*2])
-                x.append(x_bar[re_sample_index[i]*2+1])
+                spreadx = int(random.gauss(0,0.1)*spreaddis)
+                spready = int(random.gauss(0,0.1)*spreaddis)
+                x.append(x_bar[re_sample_index[i]*2]+spreadx)
+                x.append(x_bar[re_sample_index[i]*2+1]+spready)
                 px = x_bar[re_sample_index[i]*2]
                 py = x_bar[re_sample_index[i]*2+1]
                 
@@ -191,12 +216,25 @@ if __name__ == "__main__":
             x_bar=x
             deltax = dx
             deltay = dy
+            if(len(dx_list)>5):
+                deltax = dx + 0.5*(dx_list[-1] - dx_list[-2]) +  0.05*(dx_list[-2] - dx_list[-3])
+                deltay = dy + 0.5*(dy_list[-1] - dy_list[-2]) +  0.05*(dy_list[-2] - dy_list[-3])
+
+            # for i in range(len(x_bar)//2):
+            #     sx = int(random.gauss(0,0.5)*spreaddis)
+            #     sy = int(random.gauss(0,0.5)*spreaddis)
+            #     if x_bar[i*2]+ deltax+sx <768:
+            #         x_bar[i*2]+=deltax+sx
+            #     if x_bar[i*2+1]+ deltay+sy <576:
+            #         x_bar[i*2+1]+= deltay+sy
+
             for i in range(len(x_bar)//2):
-                if x_bar[i*2]+ deltax <768:
+
+                if x_bar[i*2]+ deltax<768:
                     x_bar[i*2]+=deltax
                 if x_bar[i*2+1]+ deltay <576:
                     x_bar[i*2+1]+= deltay
-                    
+
             x_avg=0
             y_avg=0
             for i in range(len(x_bar)//2):
@@ -207,8 +245,13 @@ if __name__ == "__main__":
             y_avg=y_avg/re_sampling_num
             X_posterior=np.array([[x_avg,y_avg,xywh[2],xywh[3],dx,dy]]).T
             box_posterior = xywh_to_xyxy(np.array([x_avg,y_avg,xywh[2],xywh[3]]))
+            box_center = calc_center(box_posterior)
+    
+            trace_list = updata_trace_list(box_center, trace_list, 20)
             
             x=x_bar
+            cv2.putText(frame, "Lost", (box_center[0], box_center[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                        (255, 0, 0), 2)
 
 
         draw_trace(frame, trace_list)
